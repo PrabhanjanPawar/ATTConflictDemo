506859f7b56d1aecac1febecd96aeb79412752e3
Prabhanjan <prabhanjan.pawar@gmail.com>
2024-03-08 08:46:23 +0530

    New Code Added for changes

--- src/conflictCheck/Payment.java
+++ src/conflictCheck/Payment.java
@@ -6,5 +6,7 @@ public class Payment
 	public void paymentClass()
 	{
 		System.out.println("Payment class is created but conflict raised");
+		System.out.println("need to make payment");
+		System.out.println("payment done");
 	}
 }
