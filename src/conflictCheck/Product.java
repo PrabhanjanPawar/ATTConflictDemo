e0c2bc946ec1903f6dd5e9e6c06972f8559d49a4
Prabhanjan <prabhanjan.pawar@gmail.com>
2024-03-08 08:28:07 +0530

    Changes added to branch2

--- src/conflictCheck/Product.java
+++ src/conflictCheck/Product.java
@@ -1,5 +1,10 @@
 package conflictCheck;
 
-public class Product {
+public class Product 
+{
 
+	public void productPage()
+	{
+		System.out.println("This is product page of Conflict but conflict raised");
+	}
 }
